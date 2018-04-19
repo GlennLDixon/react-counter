3d4beec3e75a09e8695c3c54fe300805a724f3f6
Glenn Dixon <glenndixon19@gmail.com>
2018-04-18 20:42:49 -0500

    first commit

---
+++ src/pages/page-3.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+export default () => (
+    <div>
+        <p>Welcome to the third page</p>
+        <Link to="/">Back Home</Link>
+    </div>
+)
